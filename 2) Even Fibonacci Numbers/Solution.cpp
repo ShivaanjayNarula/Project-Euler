7884632f0253a5eb6b9a36ee276202254b3f53de
Shivaanjay Narula <115805065+ShivaanjayNarula@users.noreply.github.com>
2024-10-11 04:56:05 +0530

    Add files via upload

---
+++ 2) Even Fibonacci Numbers/Solution.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    long long ans = 2;
+    long long a = 2;
+    long long b = 8;
+    while (b <= n)
+    {
+        ans += b;
+        //// Next even Fibonacci number
+        long long temp = 4 * b + a;
+        a = b;
+        b = temp;
+    }
+    cout << ans << endl;
+    return 0;
+}
